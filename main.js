4bdcfd0b7874f6fc16689d094492e9b07dc1eb26
MX <dinosaurbao@pm.me>
2022-09-22 17:51:24 -0400

    added code to show the social icons and the svg's color change

---
+++ main.js
@@ -0,0 +1,11 @@
+const share = document.querySelector(".share");
+const popup = document.querySelector(".popup");
+const icon = document.getElementById("icon-share");
+
+share.addEventListener('click',()=>{
+
+    popup.style.visibility = "visible";
+    share.style.backgroundColor= "hsl(217, 19%, 35%)";
+    icon.style.fill = "white";
+    icon.classList.add("inside");
+})
